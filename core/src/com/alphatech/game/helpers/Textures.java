7de2b7bf846751da03f4acbee1905a8dc34f49cc
Yanof Nasr <i7uhoz@inf.elte.hu>
2022-03-29 23:22:10 +0200

    ALT-34 Train units with animation
    adding image buttons for the soldiers to be created in the game screen.

--- core/src/com/alphatech/game/helpers/Textures.java
+++ core/src/com/alphatech/game/helpers/Textures.java
@@ -1,6 +1,8 @@
 package com.alphatech.game.helpers;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Textures {
 
@@ -8,15 +10,36 @@ public class Textures {
     public static final Texture PLACE_HOLDER = new Texture("GroundTiles/BuildGround.png");
     public static final Texture ENDTURN_TEXT = new Texture("Texts/EndTurn.png");
 
-    // Soldiers
+    // Soldiers Buttons
     public static final Texture SOLDIER1 = new Texture("Enemy1/soldier1.png");
     public static final Texture SOLDIER3 = new Texture("Enemy3/soldier3.png");
 
-    // Red Soldiers
-    public static final Texture SOLDIER1_IDLE_RED = new Texture("Enemy1/Red/Idle/0.png");
-    public static final Texture SOLDIER3_IDLE_RED = new Texture("Enemy3/Red/Idle/0.png");
+    // Idle Red Soldiers
+    public static final TextureAtlas SOLDIER1_IDLE_RED = new TextureAtlas(
+            Gdx.files.internal("Enemy1/Red/Idle/Soldier.atlas"));
+    public static final TextureAtlas SOLDIER3_IDLE_RED = new TextureAtlas(
+            Gdx.files.internal("Enemy3/Red/Idle/Soldier.atlas"));
 
-    // Blue Soldiers
-    public static final Texture SOLDIER1_IDLE_BLUE = new Texture("Enemy1/Blue/Idle/0.png");
-    public static final Texture SOLDIER3_IDLE_BLUE = new Texture("Enemy3/Blue/Idle/0.png");
+    // Idle Blue Soldiers
+    public static final TextureAtlas SOLDIER1_IDLE_BLUE = new TextureAtlas(
+            Gdx.files.internal("Enemy1/Blue/Idle/Soldier.atlas"));
+    public static final TextureAtlas SOLDIER3_IDLE_BLUE = new TextureAtlas(
+            Gdx.files.internal("Enemy3/Blue/Idle/Soldier.atlas"));
+
+    /**
+     * Dispose the constants in the game screen
+     */
+    public static void disposeConstants() {
+
+        PLACE_HOLDER.dispose();
+        ENDTURN_TEXT.dispose();
+        SOLDIER1.dispose();
+        SOLDIER3.dispose();
+        SOLDIER1_IDLE_RED.dispose();
+        SOLDIER3_IDLE_RED.dispose();
+        SOLDIER1_IDLE_BLUE.dispose();
+        SOLDIER3_IDLE_BLUE.dispose();
+
+    }
+
 }
