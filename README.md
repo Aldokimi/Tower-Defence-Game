550f8596b8d91c90defc3061301ddf3e3f0b7f98
Yanof Nasr <i7uhoz@inf.elte.hu>
2022-04-04 10:13:35 +0200

    ALT-70 Add unit testing for units

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Alpha Tech
+Evolve your gaming experience
 
+## Quickstart
+
+### Prerequisites
+
+- JDK 8+<br>
+- Gradle 6.7.1
+
+### Build
+```bash
+gradle desktop:build 
+```
+or 
+```bash
+gradle build 
+```
+### Run the game
+```bash
+gradle desktop:run 
+```
+or 
+```bash
+gradle run 
+```
+
+### Run unit tests
+```bash
+gradle test
+```
+or
+```bash
+gradle check
+```
+or 
+```bash
+gradle :core:test
+```
+
+## Unit testing artifacts
+Artifacts can be downloaded from CICD -> Pipelines 
--- core/src/com/alphatech/game/helpers/Textures.java
+++ core/src/com/alphatech/game/helpers/Textures.java
@@ -6,40 +6,40 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Textures {
 
-    // Map
-    public static final Texture PLACE_HOLDER = new Texture("GroundTiles/BuildGround.png");
-    public static final Texture ENDTURN_TEXT = new Texture("Texts/EndTurn.png");
+        // Map
+        public static final Texture PLACE_HOLDER = new Texture("GroundTiles/BuildGround.png");
+        public static final Texture ENDTURN_TEXT = new Texture("Texts/EndTurn.png");
 
-    // Soldiers Buttons
-    public static final Texture SOLDIER1 = new Texture("Enemy1/soldier1.png");
-    public static final Texture SOLDIER3 = new Texture("Enemy3/soldier3.png");
+        // Soldiers Buttons
+        public static final Texture SOLDIER1 = new Texture("Enemy1/soldier1.png");
+        public static final Texture SOLDIER3 = new Texture("Enemy3/soldier3.png");
 
-    // Idle Red Soldiers
-    public static final TextureAtlas SOLDIER1_IDLE_RED = new TextureAtlas(
-            Gdx.files.internal("Enemy1/Red/Idle/Soldier.atlas"));
-    public static final TextureAtlas SOLDIER3_IDLE_RED = new TextureAtlas(
-            Gdx.files.internal("Enemy3/Red/Idle/Soldier.atlas"));
+        // Idle Red Soldiers
+        public static final TextureAtlas SOLDIER1_IDLE_RED = new TextureAtlas(
+                        Gdx.files.internal("Enemy1/Red/Idle/Soldier.atlas"));
+        public static final TextureAtlas SOLDIER3_IDLE_RED = new TextureAtlas(
+                        Gdx.files.internal("Enemy3/Red/Idle/Soldier.atlas"));
 
-    // Idle Blue Soldiers
-    public static final TextureAtlas SOLDIER1_IDLE_BLUE = new TextureAtlas(
-            Gdx.files.internal("Enemy1/Blue/Idle/Soldier.atlas"));
-    public static final TextureAtlas SOLDIER3_IDLE_BLUE = new TextureAtlas(
-            Gdx.files.internal("Enemy3/Blue/Idle/Soldier.atlas"));
+        // Idle Blue Soldiers
+        public static final TextureAtlas SOLDIER1_IDLE_BLUE = new TextureAtlas(
+                        Gdx.files.internal("Enemy1/Blue/Idle/Soldier.atlas"));
+        public static final TextureAtlas SOLDIER3_IDLE_BLUE = new TextureAtlas(
+                        Gdx.files.internal("Enemy3/Blue/Idle/Soldier.atlas"));
 
-    /**
-     * Dispose the constants in the game screen
-     */
-    public static void disposeConstants() {
+        /**
+         * Dispose the constants in the game screen
+         */
+        public static void disposeConstants() {
 
-        PLACE_HOLDER.dispose();
-        ENDTURN_TEXT.dispose();
-        SOLDIER1.dispose();
-        SOLDIER3.dispose();
-        SOLDIER1_IDLE_RED.dispose();
-        SOLDIER3_IDLE_RED.dispose();
-        SOLDIER1_IDLE_BLUE.dispose();
-        SOLDIER3_IDLE_BLUE.dispose();
+                PLACE_HOLDER.dispose();
+                ENDTURN_TEXT.dispose();
+                SOLDIER1.dispose();
+                SOLDIER3.dispose();
+                SOLDIER1_IDLE_RED.dispose();
+                SOLDIER3_IDLE_RED.dispose();
+                SOLDIER1_IDLE_BLUE.dispose();
+                SOLDIER3_IDLE_BLUE.dispose();
 
-    }
+        }
 
 }
